53645528774d0ee6544544732ebc27ec3f3ad3c3
RajithaKWG <rkrishan894@gmail.com>
2025-02-06 00:06:09 +0530

    Implemented routers

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import Home from './pages/Home';
+import Result from './pages/Result';
+import BuyCredit from './pages/BuyCredit';
+import { Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div>
-      <h1 className=''>
-        Hello, Vite + React + Tailwind CSS!
-      </h1>
+    <div className='px-4  sm:px-10 md:-14 lg:px-28 min-h-screen'>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/result" element={<Result />} />
+        <Route path="/buy-credit" element={<BuyCredit />} />
+      </Routes>
     </div>
   )
 }
