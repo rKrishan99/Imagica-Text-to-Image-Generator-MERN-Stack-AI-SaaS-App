114ab290be69de623f5d9b87fc5f703f516ffdce
RajithaKWG <rkrishan894@gmail.com>
2025-02-07 13:46:39 +0530

    created userModek in model

--- server/model/userModel.js
+++ server/model/userModel.js
@@ -20,4 +20,6 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-const userModel = mongoose.model("user", userSchema);
+const userModel = mongoose.models.user || mongoose.model("user", userSchema);
+
+export default userModel;
