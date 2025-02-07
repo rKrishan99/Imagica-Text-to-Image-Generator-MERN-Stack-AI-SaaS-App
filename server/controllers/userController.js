9b2bc2dd4f1bc107c534b8306481573659fc383f
RajithaKWG <rkrishan894@gmail.com>
2025-02-07 14:01:29 +0530

    created register function

---
+++ server/controllers/userController.js
@@ -0,0 +1,39 @@
+import userModel from "../model/userModel";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Please fill in all fields" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const userData = {
+      name,
+      email,
+      password: hashedPassword,
+    };
+
+    const newUser = userModel(userData);
+    const user = await newUser.save();
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+
+    res.json({
+      success: true,
+      token,
+      user: {
+        name: user.name,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+};
